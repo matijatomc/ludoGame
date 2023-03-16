d9a68b8b0cbc94e9f7b5683f45f36e559e8bd56e
matijatomc <55134628+matijatomc@users.noreply.github.com>
2023-03-16 18:45:46 +0100

    Add files via upload

--- trbojevicTomcCovjeceNeLjutiSe/trbojevicTomcCovjeceNeLjutiSe/Igra.xaml.cs
+++ trbojevicTomcCovjeceNeLjutiSe/trbojevicTomcCovjeceNeLjutiSe/Igra.xaml.cs
@@ -23,5 +23,69 @@ namespace trbojevicTomcCovjeceNeLjutiSe
         {
             InitializeComponent();
         }
+
+        private void btnBaciKocku_Click(object sender, RoutedEventArgs e)
+        {
+            Random random= new Random();
+            int brojNaKocki = random.Next(1, 7);
+
+            BitmapImage slikaKocke = new BitmapImage();
+            ImageBrush imageBrush = new ImageBrush();
+
+
+            switch (brojNaKocki)
+            {
+                case 1:
+                    slikaKocke.BeginInit();
+                    slikaKocke.UriSource = new Uri("/Slike/Kocka/1.png", UriKind.RelativeOrAbsolute);
+                    slikaKocke.EndInit();
+                    kocka.Source = slikaKocke;
+                    imageBrush.ImageSource = slikaKocke;
+                    btnBaciKocku.Background = imageBrush;
+                    
+                    break;
+                case 2:
+                    slikaKocke.BeginInit();
+                    slikaKocke.UriSource = new Uri("/Slike/Kocka/2.png", UriKind.RelativeOrAbsolute);
+                    slikaKocke.EndInit();
+                    kocka.Source = slikaKocke;
+                    imageBrush.ImageSource = slikaKocke;
+                    btnBaciKocku.Background = imageBrush;
+                    break;
+                case 3:
+                    slikaKocke.BeginInit();
+                    slikaKocke.UriSource = new Uri("/Slike/Kocka/3.png", UriKind.RelativeOrAbsolute);
+                    slikaKocke.EndInit();
+                    kocka.Source = slikaKocke;
+                    imageBrush.ImageSource = slikaKocke;
+                    btnBaciKocku.Background = imageBrush;
+                    break;
+                case 4:
+                    slikaKocke.BeginInit();
+                    slikaKocke.UriSource = new Uri("/Slike/Kocka/4.png", UriKind.RelativeOrAbsolute);    
+                    slikaKocke.EndInit();
+                    kocka.Source = slikaKocke;
+                    imageBrush.ImageSource = slikaKocke;
+                    btnBaciKocku.Background = imageBrush;
+                    break;
+                case 5:
+                    slikaKocke.BeginInit();
+                    slikaKocke.UriSource = new Uri("/Slike/Kocka/5.png", UriKind.RelativeOrAbsolute);
+                    slikaKocke.EndInit();
+                    kocka.Source = slikaKocke;
+                    imageBrush.ImageSource = slikaKocke;
+                    btnBaciKocku.Background = imageBrush;
+                    break;
+                case 6:
+                    slikaKocke.BeginInit();
+                    slikaKocke.UriSource = new Uri("/Slike/Kocka/6.png", UriKind.RelativeOrAbsolute);
+                    slikaKocke.EndInit();
+                    kocka.Source = slikaKocke;
+                    imageBrush.ImageSource = slikaKocke;
+                    btnBaciKocku.Background = imageBrush;
+                    break;
+            }
+            txt.Text = Convert.ToString(brojNaKocki);
+        }
     }
 }
